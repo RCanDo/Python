bc3af76b018583b545700e9e072c8ab4d1a89097
RCanDo <rcando@int.pl>
2020-07-13 14:32:11 +0200

    plotly 03

--- graphics/Plotly/lines_on_mapbox.py
+++ graphics/Plotly/lines_on_mapbox.py
@@ -48,6 +48,58 @@ pd.set_option('display.max_rows', 150)
 
 #%%
 import plotly.express as px
+px.set_mapbox_access_token('pk.eyJ1IjoibWFybGVuYWR1ZGEiLCJhIjoiY2tjYW1kY2g0MXU5ZjJzcXA0MWN4dWIwYSJ9.ZSaNpYw_pWxdaY9gMNNTpQ')
 
 import plotly.io as pio
 pio.renderers
+
+#%%
+us_cities = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/us-cities-top-1k.csv")
+us_cities = us_cities.query("State in ['New York', 'Ohio']")
+us_cities.head()
+
+#%% using Plotly Express
+
+fig = px.line_mapbox(us_cities, lat="lat", lon="lon", color="State", zoom=3, height=500)
+
+fig.update_layout(mapbox_style="stamen-terrain", mapbox_zoom=4, mapbox_center_lat = 41,
+    margin={"r":0,"t":0,"l":0,"b":0})
+
+fig.show(renderer='firefox')
+
+#%% using Scattermapbox traces
+
+import plotly.graph_objects as go
+
+pio.orca.config.mapbox_access_token = 'pk.eyJ1IjoibWFybGVuYWR1ZGEiLCJhIjoiY2tjYW1kY2g0MXU5ZjJzcXA0MWN4dWIwYSJ9.ZSaNpYw_pWxdaY9gMNNTpQ'
+
+fig = go.Figure(go.Scattermapbox(
+    mode = "markers+lines",
+    lon = [10, 20, 30],
+    lat = [10, 20, 30],
+    marker = {'size': 10}))
+
+fig.add_trace(go.Scattermapbox(
+    mode = "markers+lines",
+    lon = [-50, -60,40],
+    lat = [30, 10, -20],
+    marker = {'size': 10}))
+
+fig.update_layout(
+    margin ={'l':0,'t':0,'b':0,'r':0},
+    mapbox = {
+        'center': {'lon': 10, 'lat': 10},
+        'style': "stamen-terrain",
+        'center': {'lon': -20, 'lat': -20},
+        'zoom': 1})
+
+fig.show()
+
+
+#%%
+
+
+#%%
+
+
+#%%
