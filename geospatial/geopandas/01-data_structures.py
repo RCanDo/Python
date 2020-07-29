aba09170d6fe27b7eb29049cb3cf81e3a2aa4f7b
RCanDo <rcando@int.pl>
2020-07-29 17:52:48 +0200

    geospatial sth

--- geospatial/geopandas/01-data_structures.py
+++ geospatial/geopandas/01-data_structures.py
@@ -43,22 +43,11 @@ import matplotlib.pyplot as plt
 
 #%%
 
-pd.options.display.width = 0  # autodetects the size of your terminal window
+pd.set_option("display.max_columns", None)
+pd.set_option("display.max_rows", None)
+pd.set_option('display.max_seq_items', None)
 
-pd.set_option('display.max_rows', 500)
-pd.options.display.max_rows = 500         # the same
-pd.options.display.max_colwidth = 500         # the same
-
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
-
-pd.set_option('display.max_rows', 500)   #!!!
-
-pd.set_option('display.max_columns', None)
-pd.set_option('display.expand_frame_repr', False)
-pd.set_option('max_colwidth', -1)
-
-pd.set_option('display.precision', 2)
+pd.set_option('display.precision', 3)
 
 # %%
 world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
@@ -69,7 +58,7 @@ world.geometry.name   # 'geometry'
 
 world['name']
 world.loc[world['name'] == 'Poland', :]
-world.loc[world['name'] == 'Poland', 'states']
+world.loc[world['name'] == 'Poland', 'geometry']
 
 
 #%%
